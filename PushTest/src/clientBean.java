743f2a49aa3f7ea026e95e186f2daa8dab686396
lookueye <lookmyeye@gmail.com>
2020-10-28 16:33:47 +0800

    test

---
+++ PushTest/src/clientBean.java
@@ -0,0 +1,35 @@
+package client.model;
+
+public class clientBean implements java.io.Serializable{
+	private static final long serialVersionUID = 1L;
+	private Integer memberNo ;		
+	private String  account;	
+	private String  password;
+	private String  gender;		
+	private String  email;		
+	private Integer picture;	
+	private String  fullName;	
+	private String  nickname;	
+	private String  phone;		
+	private Integer birthYear;	
+	private Integer birthMonth;	
+	private String starSign;	
+	private String bloodType;	
+	private String country;		
+	private String address;		
+	private String hobby;		
+	private String education;	
+	private String school;		
+	private String company;		
+	private String job;		
+	private String income;		
+	private String introduction;	
+	private Integer level;		
+	private Integer emp;		
+	private Integer sighTrip;	
+	private Integer postTrip;	
+	private Integer finishTrip;	
+	private Integer coin;		
+	private Integer point;
+	
+}
