279080fe973b0b777ccab78230a6dd427d4cd758
WH Yang <zero60611@gmail.com>
2020-10-28 13:08:48 +0800

    hi

---
+++ PushTest/src/method.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class method {
+	public static void main(String[] args){ 
+		int n = 0, sum = 0; 
+		while (sum < 20) { 
+			n++; 
+			sum+=n;
+			} System.out.println(sum);
+		}
+}
+
